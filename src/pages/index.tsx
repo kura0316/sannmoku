ddd406bb6ec7704bc92916fd7fb55b2091680d2c
kura0316 <0316kuracchi@gmail.com>
2025-02-07 01:51:49 +0900

    irokaer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,35 @@ const Home = () => {
   const [block2, setBlock2] = useState(0);
   const [block3, setBlock3] = useState(0);
 
+  const handleIncrement1 = () => {
+    setBlock1(block1 === 0 ? 1 : 0);
+  };
+  const handleIncrement2 = () => {
+    setBlock2(block2 === 0 ? 1 : 0);
+  };
+  const handleIncrement3 = () => {
+    setBlock3(block3 === 0 ? 1 : 0);
+  };
+
   return (
     <div className={styles.container}>
-      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
+      <div
+        style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }}
+        onClick={handleIncrement1}
+        className={styles.block}
+      />
 
-      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
+      <div
+        style={{ backgroundColor: block2 === 0 ? 'red' : 'blue' }}
+        onClick={handleIncrement2}
+        className={styles.block}
+      />
 
-      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
+      <div
+        style={{ backgroundColor: block3 === 0 ? 'red' : 'blue' }}
+        onClick={handleIncrement3}
+        className={styles.block}
+      />
     </div>
   );
 };
