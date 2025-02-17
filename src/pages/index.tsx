e661eef09f38d8ddacde6fac5876aa0b0574f747
kura0316 <0316kuracchi@gmail.com>
2025-02-18 05:15:37 +0900

    色

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,34 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [block1, setBlock1] = useState(0);
-  const [block2, setBlock2] = useState(0);
-  const [block3, setBlock3] = useState(0);
+  const [blocks, setBlocks] = useState([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ]);
 
-  const handleIncrement1 = () => {
-    setBlock1(block1 === 0 ? 1 : 0);
-  };
-  const handleIncrement2 = () => {
-    setBlock2(block2 === 0 ? 1 : 0);
-  };
-  const handleIncrement3 = () => {
-    setBlock3(block3 === 0 ? 1 : 0);
+  const handleClick1 = (x: number, y: number) => {
+    const newBlocks = structuredClone(blocks);
+    newBlocks[y][x] = blocks[y][x] === 0 ? 1 : 0;
+    setBlocks(newBlocks);
   };
 
   return (
     <div className={styles.container}>
-      <div
-        style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }}
-        onClick={handleIncrement1}
-        className={styles.block}
-      />
-
-      <div
-        style={{ backgroundColor: block2 === 0 ? 'red' : 'blue' }}
-        onClick={handleIncrement2}
-        className={styles.block}
-      />
-
-      <div
-        style={{ backgroundColor: block3 === 0 ? 'red' : 'blue' }}
-        onClick={handleIncrement3}
-        className={styles.block}
-      />
+      <div className={styles.board}>
+        {blocks.map((row, rowIndex) =>
+          row.map((block, blockIndex) => (
+            <div
+              key={blockIndex}
+              style={{ backgroundColor: block === 0 ? 'red' : 'white' }}
+              className={styles.block}
+              onClick={() => handleClick1(blockIndex, rowIndex)}
+            />
+          )),
+        )}
+      </div>
     </div>
   );
 };
